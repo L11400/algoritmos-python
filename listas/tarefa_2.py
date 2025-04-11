074c2310763c4b10721939cabd09e0c404ba8754
Cecília <analiaceciliasilva033@gmail.com>
2025-04-11 20:23:25 -0300

    atualização

--- listas/tarefa_2.py
+++ listas/tarefa_2.py
@@ -4,4 +4,4 @@ for i in range (10):
    numeros.append(float(input(f"Digite o {i+1}°numero real:")))
 print("Os numeros inversos são:", end="")
 for numero in numeros[::-1]:
-    print(f" {numero}",end="")5
+    print(f" {numero}",end="")
